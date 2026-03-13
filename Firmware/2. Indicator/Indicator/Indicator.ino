bd70dbf2fc7c230671661d0c5193d7e8443be23d
m1cha3l-s <254994195+m1cha3l-s@users.noreply.github.com>
2026-03-13 17:47:02 +0100

    Firmware for Remote and Indicator

---
+++ Firmware/2. Indicator/Indicator/Indicator.ino
@@ -0,0 +1,59 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <esp_now.h>
+#include <Adafruit_NeoPixel.h>
+
+  //set pins and number of leds for neopixel
+Adafruit_NeoPixel indicator(9, 3, NEO_GRB + NEO_KHZ800);
+
+  //esp-now stuff
+uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0xC7, 0xFC, 0xB0};   //MAC Address format 11:22:33:44:55:66 (MAC of your remote for sending back which color is shown every time it's changed)
+#define CHANNEL 0   //channel which esp-now uses
+
+float Remote_set;
+
+typedef  struct struck_message {
+  float set;
+  float get;
+} struck_message;
+
+struck_message Indicator_data;
+struck_message Remote_data;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println("Delivering");
+}
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&Remote_data, incomingData, sizeof(Remote_data));
+  Remote_set = Remote_data.set;
+  Serial.println("Data recv: ");
+  Serial.println(Remote_data.set);
+  indicator.fill(Remote_data.set, 0, 9);
+  indicator.show();
+  uint32_t currentColor = indicator.getPixelColor(0);
+  Indicator_data.get = currentColor;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Indicator_data, sizeof(Indicator_data));
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  esp_now_init();
+  peerInfo.channel = CHANNEL;
+  peerInfo.encrypt = false;
+
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+
+  esp_now_add_peer(&peerInfo);
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+  indicator.begin();
+  indicator.fill(0, 0, 9);
+  indicator.show();
+}
+
+void loop(){}
---
+++ Firmware/3. Remote/Remote/Remote.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <esp_now.h>
+#include <Adafruit_NeoPixel.h>
+
+
+int button1 = 3;
+int button2 = 2;
+int button3 = 2;
+
+Adafruit_NeoPixel remote(7, 0, NEO_GRB + NEO_KHZ800);
+
+  //esp-now stuff
+uint8_t broadcastAddress[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};   //MAC Address format 11:22:33:44:55:66 (MAC of your remote for sending back which color is shown every time it's changed)
+#define CHANNEL 0   //channel which esp-now uses
+
+float Indicator_current;
+
+typedef  struct struck_message {
+  float set;
+  float get;
+} struck_message;
+
+struck_message Indicator_data;
+struck_message Remote_data;
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println("Delivering");
+}
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&Indicator_data, incomingData, sizeof(Indicator_data));
+  Indicator_current = Indicator_data.get;
+  Serial.println("Data recv: ");
+  Serial.println(Indicator_data.get);
+  remote.fill(Indicator_data.get, 3, 4);
+  remote.show();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+
+  WiFi.mode(WIFI_STA);
+
+  esp_now_init();
+  peerInfo.channel = CHANNEL;
+  peerInfo.encrypt = false;
+
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+
+  esp_now_add_peer(&peerInfo);
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+  remote.begin();
+  remote.fill(0, 3, 4);
+  remote.setPixelColor(0, remote.Color(255, 0, 0));
+  remote.setPixelColor(1, remote.Color(255, 95, 31));
+  remote.setPixelColor(2, remote.Color(0, 255, 0));
+  remote.show();
+}
+
+void loop(){
+  if (digitalRead(3) == LOW) {
+    Remote_data.set = remote.Color(255, 0, 0);
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Remote_data, sizeof(Remote_data));
+    delay(1200);
+  } 
+  else if (digitalRead(2) == LOW) {
+    Remote_data.set = remote.Color(255, 95, 31);
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Remote_data, sizeof(Remote_data));
+    delay(1200);
+  }
+  else if (digitalRead(1) == LOW) {
+    Remote_data.set = remote.Color(0, 255, 0);
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Remote_data, sizeof(Remote_data));
+    delay(1200);
+  }
+}
